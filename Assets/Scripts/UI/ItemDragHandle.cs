d5cb7f92c7817d93c8c1077fbb8d3130d5d66306
Tyrabajojajo <tyraszymon87@gmail.com>
2026-03-28 11:26:03 +0100

    Item drop

--- Assets/Scripts/UI/ItemDragHandle.cs
+++ Assets/Scripts/UI/ItemDragHandle.cs
@@ -5,6 +5,9 @@ public class ItemDragHandle : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
 {
     Transform originalParent;
     CanvasGroup canvasGroup;
+
+    public float minDropDistance = 2f;
+    public float maxDropDistance = 3f;
     void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
@@ -61,10 +64,47 @@ public class ItemDragHandle : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
         }
         else
         {
+            //if were dropping is not within the inventory
+            if (!isWithinInventory(eventData.position))
+            {
+                //drop out item
+                DropItem(originalSlot);
+
+            }
+            else
+            {
+                //snap back to og slot
+                transform.SetParent(originalParent);
+            }
             //No slot under drop point
             transform.SetParent(originalParent);
         }
 
         GetComponent<RectTransform>().anchoredPosition = Vector2.zero; //Center
+    }
+    bool isWithinInventory(Vector2 mousePosition)
+    {
+       RectTransform inventoryRect = originalParent.parent.GetComponent<RectTransform>();
+        return RectTransformUtility.RectangleContainsScreenPoint(inventoryRect, mousePosition);
+    }
+    void DropItem(Slot orignalSlot)
+    {
+       orignalSlot.currentItem = null;
+        //find player
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+        if(playerTransform == null)
+        {
+            Debug.LogError("missing player tag");
+            return;
+        }
+        //random drop position
+        Vector2 dropOffset = Random.insideUnitCircle.normalized * Random.Range(minDropDistance, maxDropDistance);
+        Vector2 dropPosition = (Vector2)playerTransform.position + dropOffset;
+
+        //instantie drop item
+        Instantiate(gameObject, dropPosition, Quaternion.identity);
+
+        //destroy the UI one
+        Destroy(gameObject);
     }
 }
